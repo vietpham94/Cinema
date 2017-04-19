6fb9c7c2f03644928eeeee4caa52e0cc542c5260
kienctn15 <kienctn15@gmail.com>
2017-04-19 18:41:32 +0700

    18:41_19_04_2017
    18:41_19_04_2017

--- app/src/main/java/com/example/vietd/cinema/MovieDetail.java
+++ app/src/main/java/com/example/vietd/cinema/MovieDetail.java
@@ -1,10 +1,19 @@
 package com.example.vietd.cinema;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.text.Html;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,13 +24,20 @@ import com.google.android.youtube.player.YouTubePlayer.Provider;
 import com.google.android.youtube.player.YouTubePlayerView;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+
 //import static android.provider.MediaStore.Video.Thumbnails.VIDEO_ID;
 //YouTubeBaseActivity
 
-public class MovieDetail extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener{
+public class MovieDetail extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
 
     public String API_KEY = "AIzaSyBZj3624Oh1C0-sib0dqQn0xPGqdEA5LCk";
     public String VIDEO_ID = "rJdTUnrrISk";
+    private DrawerLayout mDrawerLayout;
+    private LinearLayout navigation_draw;
+    private ListView lv_menu;
+    private item_menu_adpter menu_adpter;
+    private ArrayList<item_menu_info> arrayMenu;
     private TextView name, duration, director, actor, country, language, genres, date, imdb, format, content;
     private ImageView poster;
     private YouTubePlayerView trailer;
@@ -49,44 +65,88 @@ public class MovieDetail extends YouTubeBaseActivity implements YouTubePlayer.On
         content = (TextView) findViewById(R.id.txt_movie_detail_content);
         btn_Booking = (Button) findViewById(R.id.btn_movie_detail_booking);
 
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.mDrawerLayout);
+        navigation_draw = (LinearLayout) findViewById(R.id.navigation_draw);
+        lv_menu = (ListView) findViewById(R.id.lv_menu);
+        createDraw();
+
         Intent i = getIntent();
+        Resources res = getResources();
+
         name.setText(i.getStringExtra("name"));
-        duration.setText("Duration: " + i.getStringExtra("duration") + " mins");
-        director.setText("Director: " + i.getStringExtra("director"));
-        actor.setText("Actors and Actresses: " + i.getStringExtra("actornactress"));
-        country.setText("Nation: " + i.getStringExtra("nation"));
-        language.setText("Language: " + i.getStringExtra("language"));
-        genres.setText("Genres: " + i.getStringExtra("category"));
-        date.setText("Release date: " + i.getStringExtra("startday"));
-        imdb.setText("IMDb: " + i.getStringExtra("imdb") + "/10");
-        if (i.getStringExtra("format") == "1") {
-            format.setText("Format: 2D");
+        if (Build.VERSION.SDK_INT >= 24) {
+            duration.setText(Html.fromHtml(res.getString(R.string.duration), Html.FROM_HTML_MODE_LEGACY) + i.getStringExtra("duration") + " phút");
         } else {
-            format.setText("Format: 3D");
+            duration.setText(Html.fromHtml(res.getString(R.string.duration)) + i.getStringExtra("duration") + " phút");
         }
+        director.setText(Html.fromHtml(res.getString(R.string.director)) + i.getStringExtra("director"));
+        actor.setText(Html.fromHtml(res.getString(R.string.actor)) + i.getStringExtra("actornactress"));
+        country.setText(Html.fromHtml(res.getString(R.string.nation)) + i.getStringExtra("nation"));
+        language.setText(Html.fromHtml(res.getString(R.string.language)) + i.getStringExtra("language"));
+        genres.setText(Html.fromHtml(res.getString(R.string.genre)) + i.getStringExtra("category"));
+        date.setText(Html.fromHtml(res.getString(R.string.release)) + i.getStringExtra("startday"));
+        imdb.setText(Html.fromHtml(res.getString(R.string.imdb)) + i.getStringExtra("imdb") + "/10");
+        if (i.getStringExtra("format") == "1") {
+            format.setText(Html.fromHtml(res.getString(R.string.format)) + "2D");
+        } else {
+            format.setText(Html.fromHtml(res.getString(R.string.format)) + "3D");
+        }
+
+
         VIDEO_ID = i.getStringExtra("urltrailer");
         VIDEO_ID = VIDEO_ID.split("v=")[1];
 
         content.setText(i.getStringExtra("content"));
         trailer.initialize(API_KEY, MovieDetail.this);
 
-        /*URL url = null;
-        try {
-            url = new URL(i.getStringExtra("poster"));
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        Toast.makeText(getApplicationContext(), "" + url + "", Toast.LENGTH_SHORT).show();*/
         Picasso.with(MovieDetail.this).load(i.getStringExtra("poster")).placeholder(R.mipmap.ic_launcher).error(R.mipmap.ic_launcher).into(poster);
 
         btn_Booking.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               /* Intent intent = new Intent(MovieDetail.this, BookingScheduleActivity.class);
-                startActivity(intent);*/
+                Intent intent = new Intent(MovieDetail.this, ScheduleMovieActivity.class);
+                startActivity(intent);
             }
         });
 
+    }
+
+    public void createDraw() {
+        lv_menu = (ListView) findViewById(R.id.lv_menu);
+        arrayMenu = new ArrayList<item_menu_info>();
+        item_menu_info item = new item_menu_info(R.drawable.ic_exit_to_app_black_24dp, "Login");
+        item_menu_info item1 = new item_menu_info(R.drawable.ic_home_black_24dp, "Home");
+        item_menu_info item2 = new item_menu_info(R.drawable.ic_view_list_black_24dp, "List movies");
+        item_menu_info item3 = new item_menu_info(R.drawable.ic_info_outline_black_24dp, "CGV info");
+        item_menu_info item4 = new item_menu_info(R.drawable.ic_power_settings_new_black_24dp, "Logout");
+        arrayMenu.add(item);
+        arrayMenu.add(item1);
+        arrayMenu.add(item2);
+        arrayMenu.add(item3);
+        arrayMenu.add(item4);
+        menu_adpter = new item_menu_adpter(this, R.layout.item_menu_layout, arrayMenu);
+        lv_menu.setAdapter(menu_adpter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_menu_bar) {
+            if (mDrawerLayout.isDrawerOpen(navigation_draw)) {
+                mDrawerLayout.closeDrawer(navigation_draw);
+            } else {
+                mDrawerLayout.openDrawer(Gravity.END);
+            }
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -103,5 +163,4 @@ public class MovieDetail extends YouTubeBaseActivity implements YouTubePlayer.On
             player.cueVideo(VIDEO_ID);
         }
     }
-
 }
--- app/src/main/java/com/example/vietd/cinema/ScheduleMovieActivity.java
+++ app/src/main/java/com/example/vietd/cinema/ScheduleMovieActivity.java
@@ -1,13 +1,68 @@
 package com.example.vietd.cinema;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ScheduleMovieActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private LinearLayout navigation_draw;
+    private ListView lv_menu;
+    private item_menu_adpter menu_adpter;
+    private ArrayList<item_menu_info> arrayMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_movie);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.mDrawerLayout);
+        navigation_draw = (LinearLayout) findViewById(R.id.navigation_draw);
+        lv_menu = (ListView) findViewById(R.id.lv_menu);
+        createDraw();
+
+    }
+
+    public void createDraw() {
+        lv_menu = (ListView) findViewById(R.id.lv_menu);
+        arrayMenu = new ArrayList<item_menu_info>();
+        item_menu_info item = new item_menu_info(R.drawable.ic_exit_to_app_black_24dp, "Login");
+        item_menu_info item1 = new item_menu_info(R.drawable.ic_home_black_24dp, "Home");
+        item_menu_info item2 = new item_menu_info(R.drawable.ic_view_list_black_24dp, "List movies");
+        item_menu_info item3 = new item_menu_info(R.drawable.ic_info_outline_black_24dp, "CGV info");
+        item_menu_info item4 = new item_menu_info(R.drawable.ic_power_settings_new_black_24dp, "Logout");
+        arrayMenu.add(item);
+        arrayMenu.add(item1);
+        arrayMenu.add(item2);
+        arrayMenu.add(item3);
+        arrayMenu.add(item4);
+        menu_adpter = new item_menu_adpter(this, R.layout.item_menu_layout, arrayMenu);
+        lv_menu.setAdapter(menu_adpter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_menu_bar) {
+            if (mDrawerLayout.isDrawerOpen(navigation_draw)) {
+                mDrawerLayout.closeDrawer(navigation_draw);
+            } else {
+                mDrawerLayout.openDrawer(Gravity.END);
+            }
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
